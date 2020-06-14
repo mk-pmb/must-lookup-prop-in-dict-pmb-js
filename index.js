6ceb8a877cf9bd2f8741cec16478766373a7e1d3
mk-pmb <mk-pmb@users.noreply.github.com>
2020-06-14 04:20:20 +0200

    v1.0.1: Fix error message.

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ var EX, objHas = Object.prototype.hasOwnProperty, conj = require('conjunct');
 function lookup(descr, cfg, dict, key, dflt) {
   var opt = (cfg && objHas.call(cfg, key) ? cfg[key] : dflt);
   if (dict && objHas.call(dict, opt)) { return dict[opt]; }
-  descr += (': ' + JSON.stringify(key) + ' must be one of '
-    + (conj.so(Object.keys(dict).map(JSON.stringify))
+  descr = (String(descr || dict) + ': ' + JSON.stringify(key)
+    + ' must be one of ' + (conj.so(Object.keys(dict).map(JSON.stringify))
       || '(Impossible choice: No valid options)') + ', not '
     + (opt ? (typeof opt) + ' ' + JSON.stringify(opt) : String(opt)));
   throw new Error(descr);
